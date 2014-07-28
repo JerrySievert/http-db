c83a615cbfd8ddf7933294687c084d5e0d807855
Jerry Sievert <code@legitimatesounding.com>
2014-07-27 18:02:44 -0700

    more passing tests

--- test/user.js
+++ test/user.js
@@ -52,6 +52,31 @@ test('creating a token works', function (t) {
   });
 });
 
+test('retrieving a token fails when a token is invalid', function (t) {
+  t.plan(3);
+
+  user.databases.tokenDB.put('test2', 'foo', function (err) {
+    t.error(err, 'no error is returned');
+    user.retrieveToken('test2', function (err, data) {
+      t.ok(err, 'an error is thrown');
+      t.ok(err.isInvalid, 'the token is invalid');
+    });
+  });
+});
+
+test('retrieving a token fails when a token is expired', function (t) {
+  t.plan(3);
+
+  user.createToken({ token: 'test3', expires: (+new Date()) - 1000 }, function (err) {
+    t.error(err, 'no error is returned');
+    user.retrieveToken('test3', function (err, data) {
+      t.ok(err, 'an error is thrown');
+      t.ok(err.isExpired, 'the token is expired');
+    });
+  });
+});
+
+
 test('creating a token fails when a user already exists', function (t) {
   t.plan(1);
 
@@ -60,11 +85,47 @@ test('creating a token fails when a user already exists', function (t) {
   });
 });
 
+test('auth against a token without a username fails', function (t) {
+  t.plan(2);
+
+  user.authStoreByToken('foo', 'test', function (err) {
+    console.log(err);
+    t.ok(err, 'an error is returned');
+    t.ok(err.isValidUsername, 'username is invalid');
+  });
+});
+
 test('closing databases works', function (t) {
   t.plan(1);
 
   user.closeDatabases(function (err) {
     t.error(err, 'no error while closing databases');
+  });
+});
+
+test('closing databases works when only the token database is open', function (t) {
+  t.plan(2);
+  user.openDatabases();
+
+  user.databases.userDB.close(function (err) {
+    user.databases.userDB = undefined;
+    t.error(err, 'no error closing the user database');
+    user.closeDatabases(function (err) {
+      t.error(err, 'no error while closing databases');
+    });
+  });
+});
+
+test('closing databases works when only the user database is open', function (t) {
+  t.plan(2);
+  user.openDatabases();
+
+  user.databases.tokenDB.close(function (err) {
+    user.databases.tokenDB = undefined;
+    t.error(err, 'no error closing the user database');
+    user.closeDatabases(function (err) {
+      t.error(err, 'no error while closing databases');
+    });
   });
 });
 
