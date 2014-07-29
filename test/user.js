60442d5b59388ecb846d259953a045e6b3082acc
Jerry Sievert <code@legitimatesounding.com>
2014-07-28 18:39:08 -0700

    back out of tokens in favor of jwt

--- test/user.js
+++ test/user.js
@@ -35,7 +35,7 @@ test('retrieving a user works', function (t) {
 test('retrieving a user fails when a user is invalid', function (t) {
   t.plan(3);
 
-  user.databases.userDB.put('test2', 'foo', function (err) {
+  user.database.userDB.put('test2', 'foo', function (err) {
     t.error(err, 'no error is returned');
     user.retrieveUser('test2', function (err, data) {
       t.ok(err, 'an error is thrown');
@@ -44,97 +44,20 @@ test('retrieving a user fails when a user is invalid', function (t) {
   });
 });
 
-test('creating a token works', function (t) {
-  t.plan(1);
-
-  user.createToken({ token: 'test', username: 'abc123' }, function (err, data) {
-    t.error(err, 'no error is returned');
-  });
-});
-
-test('retrieving a token fails when a token is invalid', function (t) {
-  t.plan(3);
-
-  user.databases.tokenDB.put('test2', 'foo', function (err) {
-    t.error(err, 'no error is returned');
-    user.retrieveToken('test2', function (err, data) {
-      t.ok(err, 'an error is thrown');
-      t.ok(err.isInvalid, 'the token is invalid');
-    });
-  });
-});
-
-test('retrieving a token fails when a token is expired', function (t) {
-  t.plan(3);
-
-  user.createToken({ token: 'test3', expires: (+new Date()) - 1000 }, function (err) {
-    t.error(err, 'no error is returned');
-    user.retrieveToken('test3', function (err, data) {
-      t.ok(err, 'an error is thrown');
-      t.ok(err.isExpired, 'the token is expired');
-    });
-  });
-});
-
-
-test('creating a token fails when a user already exists', function (t) {
-  t.plan(1);
-
-  user.createToken({ token: 'test', test: 'abc123' }, function (err, data) {
-    t.ok(err, 'an error is returned');
-  });
-});
-
-test('auth against a token without a username fails', function (t) {
-  t.plan(2);
-
-  user.authStoreByToken('foo', 'test', function (err) {
-    t.ok(err, 'an error is returned');
-    t.notOk(err.isValidUsername, 'username is invalid');
-  });
-});
 
 test('closing databases works', function (t) {
   t.plan(1);
 
-  user.closeDatabases(function (err) {
+  user.closeDatabase(function (err) {
     t.error(err, 'no error while closing databases');
   });
 });
 
-test('closing databases works when only the token database is open', function (t) {
-  t.plan(2);
-  user.openDatabases();
-
-  user.databases.userDB.close(function (err) {
-    user.databases.userDB = undefined;
-    t.error(err, 'no error closing the user database');
-    user.closeDatabases(function (err) {
-      t.error(err, 'no error while closing databases');
-    });
-  });
-});
-
-test('closing databases works when only the user database is open', function (t) {
-  t.plan(2);
-  user.openDatabases();
-
-  user.databases.tokenDB.close(function (err) {
-    user.databases.tokenDB = undefined;
-    t.error(err, 'no error closing the user database');
-    user.closeDatabases(function (err) {
-      t.error(err, 'no error while closing databases');
-    });
-  });
-});
 
 test('clean up database store', function (t) {
-  t.plan(2);
+  t.plan(1);
 
   leveldown.destroy(user.config.userDir + "/users", function (err) {
     t.error(err, 'no error on user directory destroy');
-    leveldown.destroy(user.config.userDir + "/tokens", function (err) {
-      t.error(err, 'no error on token directory destroy');
-    });
   });
 });
