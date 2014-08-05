a442dfcebe6ac2c2523ef79508a7162737c0b366
Jerry Sievert <code@legitimatesounding.com>
2014-08-04 18:37:38 -0700

    more updates

--- index.js
+++ index.js
@@ -1,83 +1,98 @@
 var Hapi = require('hapi');
 var store = require('./lib/store');
+var token = require('./lib/token');
+var user = require('./lib/user');
+
+var config = require('./config.json');
 
 // Create a server with a host and port
 var server = new Hapi.Server('localhost', 8000, { payload: { maxBytes: 1048576 * 5 } });
 
-server.pack.register(require('hapi-auth-bearer-token'), function (err) {
+server.pack.register(require('hapi-auth-jwt-request'), function (err) {
   server.auth.strategy('store', 'bearer-access-token', {
-    validateFunc: function( token, callback ) {
-      console.log(encodeURIComponent(this.params.store));
-      // Use a real strategy here,
-      // comparing with a token from your database for example
-      if(token === "1234"){
-        callback(null, true, { token: token });
-      } else {
-        callback(null, false, { token: token });
-      }
+    validateFunc: function (decoded, request, callback) {
+      token.validateToken(decoded, function (err) {
+        if (err) {
+          callback(null, false);
+        } else {
+          var store = encodeURIComponent(request.params.store);
+
+          user.authStoreByUsername (store, decoded.username, function (err) {
+            if (err) {
+              callback(null, false);
+            } else {
+              callback(null, true, decoded);
+            }
+          });
+        }
+      });
+    },
+    secret: config.token.secret
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/data/{store}/{id}',
+    config: { auth: 'store' },
+    handler: function (request, reply) {
+      store.get(encodeURIComponent(request.params.store), encodeURIComponent(request.params.id), function (err, data) {
+        if (err && err.notFound) {
+          reply('').code(204);
+        } else {
+          reply(data).type('application/json');
+        }
+      });
     }
   });
-});
 
-
-
-server.route({
-  method: 'GET',
-  path: '/data/{store}',
-  handler: function (request, reply) {
-    store.keys(encodeURIComponent(request.params.store), function (err, data) {
-      if (err) {
-        reply('').code(204);
-      } else {
-        reply(JSON.stringify(data)).type('application/json');
-      }
-    });
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/data/{store}/{id}',
-  config: { auth: 'store' },
-  handler: function (request, reply) {
-    store.get(encodeURIComponent(request.params.store), encodeURIComponent(request.params.id), function (err, data) {
-      if (err && err.notFound) {
-        reply('').code(204);
-      } else {
-        reply(data).type('application/json');
-      }
-    });
-  }
-});
-
-server.route({
-  method: 'POST',
-  path: '/data/{store}/{id}',
-  handler: function (request, reply) {
-    var data;
-
-    try {
-      data = JSON.stringify(reply.payload);
-    } catch (err) {
-      reply('{ "status": "error", "error": "payload should be JSON" }').code(400).type('application/json');
-      return;
+  server.route({
+    method: 'GET',
+    path: '/data/{store}',
+    config: { auth: 'store' },
+    handler: function (request, reply) {
+      store.keys(encodeURIComponent(request.params.store), function (err, data) {
+        if (err) {
+          reply('').code(204);
+        } else {
+          reply(JSON.stringify(data)).type('application/json');
+        }
+      });
     }
+  });
 
-    store.put(encodeURIComponent(request.params.store), encodeURIComponent(request.params.id), data, function (err) {
-      reply('{ "status": "ok" }').code(201).type('application/json');
-    });
-  }
+  server.route({
+    method: 'POST',
+    path: '/data/{store}/{id}',
+    config: { auth: 'store' },
+    handler: function (request, reply) {
+      var data;
+
+      try {
+        data = JSON.stringify(reply.payload);
+      } catch (err) {
+        reply('{ "status": "error", "error": "payload should be JSON" }').code(400).type('application/json');
+        return;
+      }
+
+      store.put(encodeURIComponent(request.params.store), encodeURIComponent(request.params.id), data, function (err) {
+        reply('{ "status": "ok" }').code(201).type('application/json');
+      });
+    }
+  });
+
+  server.route({
+    method: 'DELETE',
+    path: '/data/{store}/{id}',
+    config: { auth: 'store' },
+    handler: function (request, reply) {
+      store.del(encodeURIComponent(request.params.store), encodeURIComponent(request.params.id), function (err) {
+        reply('{ "status": "ok" }').type('application/json');
+      });
+    }
+  });
+
 });
 
-server.route({
-  method: 'DELETE',
-  path: '/data/{store}/{id}',
-  handler: function (request, reply) {
-    store.del(encodeURIComponent(request.params.store), encodeURIComponent(request.params.id), function (err) {
-      reply('{ "status": "ok" }').type('application/json');
-    });
-  }
-});
 
 // Start the server
 server.start(function () {
