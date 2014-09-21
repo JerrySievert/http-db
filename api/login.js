46446701c6a1fcf3136afdef1e6b2f037baaa849
Jerry Sievert <code@legitimatesounding.com>
2014-09-21 16:23:40 -0700

    update for public interface

---
+++ api/login.js
@@ -0,0 +1,41 @@
+var user = require('../lib/user');
+
+
+function login(request, reply) {
+  if (request.auth.isAuthenticated) {
+    return reply(JSON.stringify({
+      "status": "ok"
+    }));
+  }
+
+  if (request.payload.username === undefined) {
+    return reply(JSON.stringify({
+      "status": "error",
+      "error": "Username required"
+    }));
+  }
+
+  if (request.payload.password === undefined) {
+    return reply(JSON.stringify({
+      "status": "error",
+      "error": "Password required"
+    }));
+  }
+
+  user.login(request.payload.username, request.payload.password, function(err,
+    account) {
+    if (err) {
+      return reply(JSON.stringify({
+        "status": "error",
+        "error": "Bad Username or Password"
+      }));
+    }
+
+    request.auth.session.set(account);
+    return reply({
+      "status": "ok"
+    });
+  });
+}
+
+exports = module.exports = login;
