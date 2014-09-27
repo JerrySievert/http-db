701a82f9d9dd1dc9597561a684069f2011b4487e
Jerry Sievert <code@legitimatesounding.com>
2014-09-27 15:15:09 -0700

    refactor for context based config

--- test/store.js
+++ test/store.js
@@ -1,109 +1,117 @@
 var test = require('tape');
 var leveldown = require('leveldown');
+var context = require('../lib/context');
+
+context.config = {};
+
+context.config.dataDir = __dirname + "/store";
 
 var store = require('../lib/store');
 
-store.config.dataDir = __dirname + "/store";
-
-test('store errors on an empty value', function (t) {
+test('store errors on an empty value', function(t) {
   t.plan(1);
 
-  store.get('test', 'item', function (err, data) {
+  store.get('test', 'item', function(err, data) {
     t.ok(err, 'error is returned for empty value');
   });
 });
 
-test('store correctly puts an item', function (t) {
+test('store correctly puts an item', function(t) {
   t.plan(1);
 
-  store.put('test', 'item', 'value', function (err) {
+  store.put('test', 'item', 'value', function(err) {
     t.error(err, 'no error is returned for a put');
   });
 });
 
-test('store correctly gets an item', function (t) {
+test('store correctly gets an item', function(t) {
   t.plan(2);
 
-  store.get('test', 'item', function (err, data) {
+  store.get('test', 'item', function(err, data) {
     t.error(err, 'no error is returned for a valid get');
     t.equal(data, 'value', 'the correct value is returned');
   });
 });
 
-test('store correctly returns keys', function (t) {
+test('store correctly returns keys', function(t) {
   t.plan(2);
 
-  store.keys('test', function (err, data) {
+  store.keys('test', function(err, data) {
     t.error(err, 'no error is returned for keys');
-    t.deepEqual(data, { keys: [ 'item' ] }, 'keys are correct');
+    t.deepEqual(data, {
+      keys: ['item']
+    }, 'keys are correct');
   });
 });
 
-test('store can delete an item', function (t) {
+test('store can delete an item', function(t) {
   t.plan(1);
 
-  store.del('test', 'item', function (err) {
+  store.del('test', 'item', function(err) {
     t.error(err, 'no error is thrown');
   });
 });
 
-test('store can be closed', function (t) {
+test('store can be closed', function(t) {
   t.plan(1);
 
-  store.close('test', function (err) {
+  store.close('test', function(err) {
     t.error(err, 'no error is thrown for the close');
   });
 });
 
-test('store correctly puts an item when closed', function (t) {
+test('store correctly puts an item when closed', function(t) {
   t.plan(1);
 
-  store.put('test', 'item', 'value', function (err) {
-    store.close('test', function () {
+  store.put('test', 'item', 'value', function(err) {
+    store.close('test', function() {
       t.error(err, 'no error is returned for a put');
     });
   });
 });
 
-test('store correctly returns keys when the store is closed', function (t) {
+test('store correctly returns keys when the store is closed', function(t) {
   t.plan(3);
 
-  store.keys('test', function (err, data) {
+  store.keys('test', function(err, data) {
     t.error(err, 'no error is returned for keys');
-    t.deepEqual(data, { keys: [ 'item' ] }, 'keys are correct');
+    t.deepEqual(data, {
+      keys: ['item']
+    }, 'keys are correct');
 
-    store.close('test', function (err) {
+    store.close('test', function(err) {
       t.error(err, 'no error is returned for close');
     });
   });
 });
 
-test('store correctly deletes an item when closed', function (t) {
+test('store correctly deletes an item when closed', function(t) {
   t.plan(1);
 
-  store.del('test', 'item', function (err) {
-    store.close('test', function () {
+  store.del('test', 'item', function(err) {
+    store.close('test', function() {
       t.error(err, 'no error is returned for a delete');
     });
   });
 });
 
-test('store returns the correct results when filter is applied', function (t) {
+test('store returns the correct results when filter is applied', function(t) {
   t.plan(3);
 
-  store.put('test', '1', '{ "foo": "bar" }', function () {
-    store.put('test', '2', '{ "foo": "baz" }', function () {
-      store.filter('test', 'foo', 'bar', function (err, stream) {
+  store.put('test', '1', '{ "foo": "bar" }', function() {
+    store.put('test', '2', '{ "foo": "baz" }', function() {
+      store.filter('test', 'foo', 'bar', function(err, stream) {
         t.error(err, 'no error is returned');
 
         var count = 0;
-        stream.on('data', function (data) {
+        stream.on('data', function(data) {
           data = JSON.parse(data);
           count++;
           t.equal(data.key, '1', 'the correct key is returned');
-          store.del('test', '1', function () {
-            store.del('test', '2', function () {
-              t.equal(count, 1, 'the correct number of results are returned');
+          store.del('test', '1', function() {
+            store.del('test', '2', function() {
+              t.equal(count, 1,
+                'the correct number of results are returned');
             });
           });
 
@@ -113,65 +121,69 @@ test('store returns the correct results when filter is applied', function (t) {
   });
 });
 
-test('store returns the correct results when filter is applied and no results', function (t) {
-  t.plan(2);
+test('store returns the correct results when filter is applied and no results',
+  function(t) {
+    t.plan(2);
 
-  store.put('test', '1', '{ "foo": [1, 2] }', function () {
-    store.put('test', '2', '{ "foo": "baz" }', function () {
-      store.filter('test', 'foo', 9, function (err, stream) {
-        t.error(err, 'no error is returned');
+    store.put('test', '1', '{ "foo": [1, 2] }', function() {
+      store.put('test', '2', '{ "foo": "baz" }', function() {
+        store.filter('test', 'foo', 9, function(err, stream) {
+          t.error(err, 'no error is returned');
 
-        var count = 0;
-        stream.on('data', function (data) {
-          data = JSON.parse(data);
-          count++;
-        });
-
-        stream.on('end', function ( ) {
-          store.del('test', '1', function () {
-            store.del('test', '2', function () {
-              t.equal(count, 0, 'the correct number of results are returned');
-            });
+          var count = 0;
+          stream.on('data', function(data) {
+            data = JSON.parse(data);
+            count++;
           });
 
+          stream.on('end', function() {
+            store.del('test', '1', function() {
+              store.del('test', '2', function() {
+                t.equal(count, 0,
+                  'the correct number of results are returned');
+              });
+            });
+
+          });
         });
       });
     });
   });
-});
 
-test('store returns the correct results all is called', function (t) {
+test('store returns the correct results all is called', function(t) {
   t.plan(2);
 
-  store.put('test', '1', '{ "foo": "bar" }', function () {
-    store.all('test', function (err, stream) {
+  store.put('test', '1', '{ "foo": "bar" }', function() {
+    store.all('test', function(err, stream) {
       t.error(err, 'no error is returned');
 
-      stream.on('data', function (data) {
+      stream.on('data', function(data) {
         data = JSON.parse(data);
         t.equal(data.key, '1', 'the correct key is returned');
-        store.del('test', '1', function () {
-        });
+        store.del('test', '1', function() {});
       });
     });
   });
 });
 
-test('store returns the correct results when query is called', function (t) {
+test('store returns the correct results when query is called', function(t) {
   t.plan(3);
 
-  store.put('test', '1', '{ "foo": "bar" }', function () {
-    store.put('test', '2', '{ "foo": "baz" }', function () {
-      store.query('test', { foo: "bar" }, function (err, stream) {
+  store.put('test', '1', '{ "foo": "bar" }', function() {
+    store.put('test', '2', '{ "foo": "baz" }', function() {
+      store.query('test', {
+        foo: "bar"
+      }, function(err, stream) {
         t.error(err, 'no error is returned');
 
         var count = 0;
-        stream.on('data', function (data) {
+        stream.on('data', function(data) {
           count++;
           t.equal(data.key, '1', 'the correct key is returned');
-          store.del('test', '1', function () {
-            store.del('test', '2', function () {
-              t.equal(count, 1, 'the correct number of results are returned');
+          store.del('test', '1', function() {
+            store.del('test', '2', function() {
+              t.equal(count, 1,
+                'the correct number of results are returned');
             });
           });
 
@@ -181,18 +193,18 @@ test('store returns the correct results when query is called', function (t) {
   });
 });
 
-test('store correctly does not error when closed and not open', function (t) {
+test('store correctly does not error when closed and not open', function(t) {
   t.plan(1);
 
-  store.close('test', function (err) {
+  store.close('test', function(err) {
     t.error(err, 'no error is returned for a close');
   });
 });
 
-test('clean up database store', function (t) {
+test('clean up database store', function(t) {
   t.plan(1);
 
-  leveldown.destroy(store.config.dataDir + "/test", function (err) {
+  leveldown.destroy(store.config.dataDir + "/test", function(err) {
     t.error(err);
   });
 });
--- test/user.js
+++ test/user.js
@@ -2,121 +2,150 @@ var test = require('tape');
 var leveldown = require('leveldown');
 var levelup = require('levelup');
 var bcrypt = require('bcrypt');
+var context = require('../lib/context');
+
+context.config = {};
+
+context.config.userDir = __dirname + "/user";
 
 var user = require('../lib/user');
 
-user.config.userDir = __dirname + "/user";
-
-test('creating a user works', function (t) {
+test('creating a user works', function(t) {
   t.plan(1);
 
-  user.create({ username: 'test', test: 'abc123', password: 'mypass' }, function (err, data) {
+  user.create({
+    username: 'test',
+    test: 'abc123',
+    password: 'mypass'
+  }, function(err, data) {
     t.error(err, 'no error is returned');
   });
 });
 
-test('creating a user fails when a user already exists', function (t) {
+test('creating a user fails when a user already exists', function(t) {
   t.plan(1);
 
-  user.create({ username: 'test', test: 'abc123', password: 'mypass' }, function (err, data) {
+  user.create({
+    username: 'test',
+    test: 'abc123',
+    password: 'mypass'
+  }, function(err, data) {
     t.ok(err, 'an error is returned');
   });
 });
 
-test('retrieving a user works', function (t) {
+test('retrieving a user works', function(t) {
   t.plan(3);
 
-  user.retrieve('test', function (err, data) {
+  user.retrieve('test', function(err, data) {
     t.error(err, 'no error is returned');
     t.ok(data, 'user data is returned');
     t.equal(data.username, 'test', 'the username is correct');
   });
 });
 
-test('retrieving a user fails when a user is invalid', function (t) {
+test('retrieving a user fails when a user is invalid', function(t) {
   t.plan(3);
 
-  user.database.userDB.put('test2', 'foo', function (err) {
+  user.database.userDB.put('test2', 'foo', function(err) {
     t.error(err, 'no error is returned');
-    user.retrieve('test2', function (err, data) {
+    user.retrieve('test2', function(err, data) {
       t.ok(err, 'an error is thrown');
       t.ok(err.isInvalid, 'the user is invalid');
     });
   });
 });
 
-test('changing a password works', function (t) {
+test('changing a password works', function(t) {
   t.plan(2);
 
-  user.create({ username: 'test2', test: 'abc123', password: 'mypass' }, function (err, data) {
-    user.changePassword('test2', 'mypass', '123abc', function (err) {
+  user.create({
+    username: 'test2',
+    test: 'abc123',
+    password: 'mypass'
+  }, function(err, data) {
+    user.changePassword('test2', 'mypass', '123abc', function(err) {
       t.error(err, 'no error is returned');
-      user.retrieve('test2', function (e, u) {
-        t.ok(bcrypt.compareSync('123abc', u.password), 'passwords match');
+      user.retrieve('test2', function(e, u) {
+        t.ok(bcrypt.compareSync('123abc', u.password),
+          'passwords match');
       });
     });
   });
 });
 
-test('changing a password with a bad old password fails', function (t) {
+test('changing a password with a bad old password fails', function(t) {
   t.plan(1);
 
-  user.create({ username: 'test2', test: 'abc123', password: 'mypass' }, function (err, data) {
-    user.changePassword('test2', 'notmypass', '123abc', function (err) {
+  user.create({
+    username: 'test2',
+    test: 'abc123',
+    password: 'mypass'
+  }, function(err, data) {
+    user.changePassword('test2', 'notmypass', '123abc', function(err) {
       t.ok(err, 'an error is returned');
     });
   });
 });
 
-test('changing a password with a bad username fails', function (t) {
+test('changing a password with a bad username fails', function(t) {
   t.plan(1);
 
-  user.changePassword('badguy', 'notmypass', '123abc', function (err) {
-      t.ok(err, 'an error is returned');
+  user.changePassword('badguy', 'notmypass', '123abc', function(err) {
+    t.ok(err, 'an error is returned');
   });
 });
 
-test('authorizing against a store works', function (t) {
+test('authorizing against a store works', function(t) {
   t.plan(1);
 
-  user.create({ username: 'storetest', stores: [ 'test' ], password: 'mypass', active: true }, function (err, data) {
-    user.authStoreByUsername('test', 'storetest', function (err) {
+  user.create({
+    username: 'storetest',
+    stores: ['test'],
+    password: 'mypass',
+    active: true
+  }, function(err, data) {
+    user.authStoreByUsername('test', 'storetest', function(err) {
       t.error(err, 'no error is returned');
     });
   });
 });
 
-test('authorizing against a store fails when not allowed', function (t) {
+test('authorizing against a store fails when not allowed', function(t) {
   t.plan(1);
 
-  user.create({ username: 'storetest2', stores: [ 'test' ], password: 'mypass', active: true }, function (err, data) {
-    user.authStoreByUsername('test2', 'storetest2', function (err) {
+  user.create({
+    username: 'storetest2',
+    stores: ['test'],
+    password: 'mypass',
+    active: true
+  }, function(err, data) {
+    user.authStoreByUsername('test2', 'storetest2', function(err) {
       t.ok(err, 'an error is returned');
     });
   });
 });
 
-test('authorizing against a store fails when a user does not exist', function (t) {
+test('authorizing against a store fails when a user does not exist', function(t) {
   t.plan(1);
 
-  user.authStoreByUsername('doesnotexist', 'storetest2', function (err) {
+  user.authStoreByUsername('doesnotexist', 'storetest2', function(err) {
     t.ok(err, 'an error is returned');
   });
 });
 
-test('closing databases works', function (t) {
+test('closing databases works', function(t) {
   t.plan(1);
 
-  user.closeDatabase(function (err) {
+  user.closeDatabase(function(err) {
     t.error(err, 'no error while closing databases');
   });
 });
 
-test('clean up database store', function (t) {
+test('clean up database store', function(t) {
   t.plan(1);
 
-  leveldown.destroy(user.config.userDir + "/users", function (err) {
+  leveldown.destroy(user.config.userDir + "/users", function(err) {
     t.error(err, 'no error on user directory destroy');
   });
 });
-
