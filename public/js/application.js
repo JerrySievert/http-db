44a0b17fa3d32d78c60ba88476d2a7e32ea495a3
Jerry Sievert <code@legitimatesounding.com>
2014-10-09 21:55:42 -0700

    more server updates

--- public/js/application.js
+++ public/js/application.js
@@ -1,29 +1,33 @@
-var app = angular.module('Application', [ ]);
-app.controller('applicationController', function ($scope, $http, $window) {
+var app = angular.module('Application', []);
+app.controller('applicationController', function($scope, $http, $window) {
   $scope.display = 'Welcome';
 
-  $http.get('/api/v1/token').success(function (data) {
+  $http.get('/api/v1/token').success(function(data) {
     $http.defaults.headers.common.Authorization = "Bearer " + data.token;
 
-    $http.get('/database').success(function (data) {
+    $http.get('/database').success(function(data) {
+      console.log(data);
       $scope.database = data;
     });
 
-    $http.get('/database/stores').success(function (data) {
+    $http.get('/database/stores').success(function(data) {
       $scope.stores = data.stores;
     });
-  }).error(function (error) {
-  });
+  }).error(function(error) {});
 
-  $scope.storeClick = function (event) {
+  $scope.storeClick = function(event) {
     $scope.store = event.srcElement.firstChild.data;
 
-    $http.get('/database/all/' + $scope.store, { transformResponse: function (h, d) { return h; }}).success(function (data) {
+    $http.get('/database/all/' + $scope.store, {
+      transformResponse: function(h, d) {
+        return h;
+      }
+    }).success(function(data) {
       $scope.display = 'Keys';
 
       var parts = data.split("\n");
 
-      $scope.values = [ ];
+      $scope.values = [];
       for (var i = 0; i < parts.length; i++) {
         try {
           var part = JSON.parse(parts[i]);
@@ -35,35 +39,37 @@ app.controller('applicationController', function ($scope, $http, $window) {
     });
   };
 
-  $scope.keyClick = function (event) {
+  $scope.keyClick = function(event) {
     $scope.key = event.srcElement.firstChild.data;
-    $http.get('/database/value/' + $scope.store + '/' + $scope.key).success(function (data) {
-      $scope.value = render(null, data);
-      $scope.display = 'Value';
-    });
+    $http.get('/database/value/' + $scope.store + '/' + $scope.key).success(
+      function(data) {
+        $scope.value = render(null, data);
+        $scope.display = 'Value';
+      });
   };
 
-  $scope.editValue = function ( ) {
+  $scope.editValue = function() {
     $scope.valueError = null;
     $scope.oldValue = $scope.value;
     $scope.display = 'Edit';
   };
 
-  $scope.cancelValue = function ( ) {
+  $scope.cancelValue = function() {
     $scope.value = $scope.oldValue;
     $scope.display = 'Value';
   };
 
-  $scope.cancelEntry = function ( ) {
+  $scope.cancelEntry = function() {
     $scope.value = $scope.oldValue;
     $scope.display = 'Keys';
   };
 
-  $scope.saveValue = function ( ) {
+  $scope.saveValue = function() {
     var toSave;
     $scope.valueError = false;
 
-    if ($scope.key === "" || $scope.key === null || $scope.key === undefined) {
+    if ($scope.key === "" || $scope.key === null || $scope.key ===
+      undefined) {
       $scope.valueError = "Invalid Key";
       return;
     }
@@ -77,52 +83,56 @@ app.controller('applicationController', function ($scope, $http, $window) {
       return;
     }
 
-    $http.post('/database/value/' + $scope.store + '/' + $scope.key, $scope.value).success(function ( ){
+    $http.post('/database/value/' + $scope.store + '/' + $scope.key,
+      $scope.value).success(function() {
       $scope.display = 'none';
 
-      $http.get('/database/value/' + $scope.store + '/' + $scope.key).success(function (data) {
-        $scope.value = render(null, data);
-        $scope.display = 'Value';
-      });
+      $http.get('/database/value/' + $scope.store + '/' + $scope.key)
+        .success(function(data) {
+          $scope.value = render(null, data);
+          $scope.display = 'Value';
+        });
     });
   };
 
-  $scope.deleteValue = function ( ) {
-    $http.delete('/database/value/' + $scope.store + '/' + $scope.key).success(function () {
-      $scope.display = 'none';
+  $scope.deleteValue = function() {
+    $http.delete('/database/value/' + $scope.store + '/' + $scope.key).success(
+      function() {
+        $scope.display = 'none';
 
-      $scope.values = $scope.values.filter(function (data) {
-        if (data.key === $scope.key) {
-          return false;
-        } else {
-          return true;
-        }
+        $scope.values = $scope.values.filter(function(data) {
+          if (data.key === $scope.key) {
+            return false;
+          } else {
+            return true;
+          }
+        });
+
+        $scope.key = undefined;
+
+        $scope.display = 'Keys';
       });
-
-      $scope.key = undefined;
-
-      $scope.display = 'Keys';
-    });
   };
 
-  $scope.createEntry = function ( ) {
+  $scope.createEntry = function() {
     $scope.valueError = null;
     $scope.key = "";
     $scope.value = "";
     $scope.display = 'Create';
   };
 
-  $scope.accountToken = function ( ) {
-    $http.get('/api/v1/token').success(function (data) {
-      console.log(data);
+  $scope.accountToken = function() {
+    $http.get('/api/v1/token').success(function(data) {
       $scope.token = data.token;
+      $scope.display = 'Token';
     });
   };
 });
 
 
-function render (key, value, indent) {
-  var text = '', i;
+function render(key, value, indent) {
+  var text = '',
+    i;
 
   indent = indent || 0;
 
@@ -137,7 +147,7 @@ function render (key, value, indent) {
 
     text += "[\n";
 
-    for (var i = 0; i < value.length; i++) {
+    for (i = 0; i < value.length; i++) {
       text += render(null, value[i], indent + 2);
       if (i < value.length - 1) {
         text += ",";
@@ -149,7 +159,8 @@ function render (key, value, indent) {
       text += " ";
     }
     text += "]";
-  } else if (value !== null && typeof value === 'object' && value.toString() === '[object Object]') {
+  } else if (value !== null && typeof value === 'object' && value.toString() ===
+    '[object Object]') {
     for (i = 0; i < indent; i++) {
       text += " ";
     }
@@ -161,7 +172,7 @@ function render (key, value, indent) {
     text += "{\n";
 
     var keys = Object.keys(value);
-    for (var i = 0; i < keys.length; i++) {
+    for (i = 0; i < keys.length; i++) {
       text += render(keys[i], value[keys[i]], indent + 2);
       if (i < keys.length - 1) {
         text += ",";
